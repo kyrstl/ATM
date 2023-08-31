bca1ef977bff2ea1170d4dd15152fc1b980506c1
Jingjing <chrystalduan@gmail.com>
2023-08-31 14:45:05 -0700

    wrote constructor

---
+++ ATM.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.*;
+
+import org.w3c.dom.Node;
+
+public class ATM {
+    //private String userID;
+    //private double deps;
+    private HashMap<String,Double> map = new HashMap<String,Double>();
+
+    public ATM () {
+        
+    }
+
+    
+}
